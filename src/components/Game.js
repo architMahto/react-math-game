f16e210f5ac6080290e3dd76e643e67654cd0ecb
Archit Mahto <architmahto@gmail.com>
2018-06-26 00:43:24 -0400

    Created Answer, Button, and Stars components.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
+import Answer from './Answer';
+import Button from './Button';
+import Stars from './Stars';
+
 class Game extends Component {
   render() {
     return (
       <div>
         <h3>Play Nine</h3>
+        <Stars />
+        <Button />
+        <Answer />
       </div>
     );
   }
